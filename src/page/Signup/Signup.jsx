044a63555a338d900ebb41e6eb5299315f1c5baa
Kuntal-Gain <work.kuntalgain@gmail.com>
2024-08-09 19:39:34 +0530

    [FIX] MINOR FRONTEND ISSUE

--- src/page/Signup/Signup.jsx
+++ src/page/Signup/Signup.jsx
@@ -35,18 +35,18 @@ const Signup = () => {
   };
 
   return (
-    <div className='signup'>
-      <div className="signup-left">
+    <div className='login'>
+      <div className="log-left">
         <h2>Register</h2>
-        <p className='signup-para'>Sign up with</p>
-        <div className="signup-buttons">
-          <button><FcGoogle /></button>
-          <button><FaApple /></button>
-          <button><FaFacebook /></button>
+        <p className='sign-para'>Sign up with</p>
+        <div className="log-buttons">
+          <button><i><FcGoogle /></i></button>
+          <button><i><FaApple /></i></button>
+          <button><i><FaFacebook /></i></button>
         </div>
-        <p className='signup-para'>Or</p>
-        <p className='signup-para'>Your Details</p>
-        <div className="signup-input">
+        <p className='name-para'>Or</p>
+        <p className='name-para'>Your Details</p>
+        <div className="sign-input">
           <input
             type="text"
             placeholder='First Name'
@@ -62,7 +62,7 @@ const Signup = () => {
             required
           />
         </div>
-        <div className="signup-select">
+        <div className="sign-check">
           <p>Gender</p>
           <select
             name="gender"
@@ -75,8 +75,8 @@ const Signup = () => {
             <option value="Female">Female</option>
           </select>
         </div>
-        <p className='signup-para'>Login Details</p>
-        <div className="signup-input">
+        <p className='name-para'>Login Details</p>
+        <div className="sign-input">
           <input
             type="email"
             placeholder='Email'
@@ -92,28 +92,26 @@ const Signup = () => {
             required
           />
         </div>
-        <div className="signup-check-confirm">
+        <div className="check-confirm">
           <input type="checkbox" name="" id="" required />
           <p>By clicking 'Register' you agree to our website KicksClub Terms & Conditions, Kicks Privacy Notice and Terms & Conditions.</p>
         </div>
         {error && <p className="error-message">{error}</p>}
-        <p className='signup-acc'>Already have an account? <Link to='/login' style={{ textDecoration: 'none' }}><span>Log in</span></Link></p>
-        <button className='signup-button' type='button' onClick={handleRegister}>REGISTER</button>
+        <p className='create-acc'>Already have an account? <Link to='/login' style={{ textDecoration: 'none' }}><span>Log in</span></Link></p>
+        <button className='email' type='button' onClick={handleRegister}>REGISTER</button>
       </div>
-      <div className="signup-right">
+      <div className="sign-right">
         <h2>Join Kicks Club Get Rewarded Today.</h2>
-        <p>As a Kicks Club member, you get rewarded with what you love for doing what you love. Sign up today and receive immediate access to these Level 1 benefits:</p>
+        <p>As kicks club member you get rewarded with what you love for doing what you love. Sign up today and receive immediate access to these Level 1 benefits:</p>
         <ul>
           <li>Free Shipping</li>
-          <li>A 15% off voucher for your next purchase</li>
-          <li>Access to Members Only products and sales</li>
+          <li>A 15% off voucher for your next purchase​</li>
+          <li>Access to Members Only products and sales​</li>
           <li>Access to adidas Running and Training apps</li>
-          <li>Special offers and promotions</li>
+          <li>Special offers and promotions​</li>
         </ul>
-        <p className='signup-para'>Join now to start earning points, reach new levels, and unlock more rewards and benefits from adiClub.</p>
-        <Link to='/signup'>
-          <button className='join-club-button' type='button'>JOIN THE CLUB</button>
-        </Link>
+        <p className='para'>Join now to start earning points, reach new levels and unlock more rewards and benefits from adiClub.​</p>
+        <button className='join' type='submit'>JOIN THE CLUB</button>
       </div>
     </div>
   );
