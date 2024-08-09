4c4c0b99da64ef82f87ad9dd814eafdc00e0b064
sumitdas16 <sumitdasnaity@gmail.com>
2024-08-09 12:23:20 +0530

    "main.jsx updated"

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Route, Navigate } from 'react-router-dom';
@@ -24,22 +23,6 @@ const ProtectedRoute = ({ element }) => {
   return isAuthenticated() ? element : <Navigate to="/login" />;
 };
 
-
-
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './Layout.jsx'
-import './index.css'
-import Signup from './page/Signup/Signup.jsx'
-import Login from './page/Login/Login.jsx'
-import Hero from './components/Hero/Hero.jsx'
-import Products from './components/Products/Products.jsx'
-import Cart from './page/Cart/Cart.jsx'
-import Listing from './components/Listing/Listing.jsx'
-import ProtectedRoute from './components/ProtectedRoute.jsx'
->>>>>>> a8470df3d958e3ab4dd11553033f3c1d25d27d85
 
 const router = createBrowserRouter([
   {
@@ -68,7 +51,6 @@ const router = createBrowserRouter([
       },
       {
         path: '/listing',
-<<<<<<< HEAD
         element: <Listing/>,
       },
       {  
@@ -82,9 +64,6 @@ const router = createBrowserRouter([
       {
         path: '/shops',
         element: <Shops />
-=======
-        element: <Listing />
->>>>>>> a8470df3d958e3ab4dd11553033f3c1d25d27d85
       }
     ]
   }
